26fb5cd257b5de4c5c3aecf297bec19cfde2661a
Carlos Morales <carlosdmzapata@gmail.com>
2022-10-12 16:00:52 -0500

    avance2

--- tienda.js
+++ tienda.js
@@ -1,3 +1,13 @@
+function tienda() {
+    let consultaCompra = prompt("Desea realizar una compra?").toLowerCase();
+    if(consultaCompra ==`si`){
+        seleccion1 = parseInt(prompt(`${listadoNombresProductos.join(`\n`)}`) );
+    }else if(consultaCompra == `no`){
+        alert("Muchas gracias por su visita");
+    }
+    }
+
+
 function Producto(nombre, precio, stock){
     this.nombre = nombre;
     this.precio = precio;
@@ -16,7 +26,13 @@ let productoC = new Producto("Llavero", 8.50 , 200)
 
 let precioTotal = 0
 
-alert("Artículos de nuestro catálogo: \n - Mug\n - Figura\n - Llavero")
+const listaProducto =[] 
+listaProducto.push(new Producto(`Camiseta`,165035,15,1.21,1));
+listaProducto.push(new Producto(`Gorra`,6198.1,30,1.21,2));
+listaProducto.push(new Producto(`Parche`,6500,50,1.21,3));
+listaProducto.push(new Producto(`Manilla`,45000,53,1.21,4));
+
+alert("Artículos de nuestro catálogo: \n - Mug\n - Figura\n - Llavero\n - Camiseta\n - Gorra\n - Parche\n - Manilla")
 
 function calculoPrecio(cantidad, precio){
     precioTotal += (cantidad * precio)
@@ -31,13 +47,12 @@ function calculoStock(cantidad, stock, precio){
     }
 }
 
-/**/ 
 
 let cantidadCompra = parseInt(prompt("Cuantas referencias de producto desea adquirir:"))
 
 for(let i = 0; i < cantidadCompra; i = i + 1){
 
-    let productoCompra = prompt("Ingrese el nombre del producto que desea comprar: \n - Mug\n - Figura\n - Llavero")
+    let productoCompra = prompt("Ingrese el nombre del producto que desea comprar: \n - Mug\n - Figura\n - Llavero\n - Camiseta\n - Gorra\n - Parche\n - Manilla")
     
     if(productoCompra.toUpperCase() == 'MUG'){
         let cantidadProductoMug = prompt("ingrese que cantidad de " + productoA.nombre + " desea comprar:")
@@ -54,6 +69,31 @@ for(let i = 0; i < cantidadCompra; i = i + 1){
         calculoStock(cantidadProductoLlavero, stockProductoC, precioProductoC)
         productoC.restarStock(cantidadProductoLlavero)
     }
+
+    else if(productoCompra == 'Camiseta'){
+        let cantidadProductoLlavero = prompt("ingrese que cantidad de " + productoC.nombre + " desea comprar:")
+        calculoStock(cantidadProductoLlavero, stockProductoC, precioProductoC)
+        productoC.restarStock(cantidadProductoLlavero)
+    }
+
+    else if(productoCompra == 'Gorra'){
+        let cantidadProductoLlavero = prompt("ingrese que cantidad de " + productoC.nombre + " desea comprar:")
+        calculoStock(cantidadProductoLlavero, stockProductoC, precioProductoC)
+        productoC.restarStock(cantidadProductoLlavero)
+    }
+
+    else if(productoCompra == 'Manilla'){
+        let cantidadProductoLlavero = prompt("ingrese que cantidad de " + productoC.nombre + " desea comprar:")
+        calculoStock(cantidadProductoLlavero, stockProductoC, precioProductoC)
+        productoC.restarStock(cantidadProductoLlavero)
+    }
+
+    else if(productoCompra == 'Parche'){
+        let cantidadProductoLlavero = prompt("ingrese que cantidad de " + productoC.nombre + " desea comprar:")
+        calculoStock(cantidadProductoLlavero, stockProductoC, precioProductoC)
+        productoC.restarStock(cantidadProductoLlavero)
+    }
+
     else{
         alert("El producto seleccionado no se encuentra disponible")
     }
